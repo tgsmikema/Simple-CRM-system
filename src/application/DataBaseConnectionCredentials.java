6de811e3c4feaf75901b07f106760f541e076aef
tgsmikema <32582220+tgsmikema@users.noreply.github.com>
2022-01-19 17:00:01 +0800

    Update DataBaseConnectionCredentials.java

--- src/application/DataBaseConnectionCredentials.java
+++ src/application/DataBaseConnectionCredentials.java
@@ -3,22 +3,20 @@ package application;
 public class DataBaseConnectionCredentials {
 	
 	
-	public static String URL = "jdbc:mysql://MYSQL5045.site4now.net:3306/db_a7f545_crm";
-	public static String username = "a7f545_crm";
-	public static String password = "makemoney2022";
+	//public static String URL = "jdbc:mysql://MYSQL5045.site4now.net:3306/db_a7f545_crm";
+	//public static String username = "a7f545_crm";
+	//public static String password = "makemoney2022";
 	
 	//public static String URL = "jdbc:mysql://sql4.freesqldatabase.com:3306/sql4466569";
 	//public static String username = "sql4466569";
 	//public static String password = "792Zrxau3M";
 	
 	
-	
-	/*
 	// local database
 	public static String URL = "jdbc:mysql://127.0.0.1:3306/super_chat_pal_crm";
 	public static String username = "root";
 	public static String password = "masiqi93";
-	*/
+	
 }
 
 
